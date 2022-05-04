8d2d7989cbdb8ef9f9db6d550a131c090690d779
hayashidalex <mamihayashida@gmail.com>
2022-05-04 15:49:54 -0400

    Adding bootstrap elements to control layout.

--- app.py
+++ app.py
@@ -16,77 +16,109 @@ import miscellaneous_functions as misc
 
 data_path='data/36_54_54/'
 
+
+##########
+
 subvolume = misc.stack_to_numpy(data_path)
 vol_fig = misc.plotly_volume_rendering(subvolume)
 
 
 ###########
 
-# TODO: other options: color scheme, min/max
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-#app= Dash(__name__)
 
-app.layout = html.Div(children=[
-    html.H2(
-        children=f'{data_path}',
-        style={
-            'textAlign': 'center'
-            }
-    ),
-    html.Div(children=[
-        html.H4('x-slice', style={'textAlign': 'center'}),
-        dcc.Graph(id='x-slice'),
-        dcc.Slider(
-            min=0,
-            max=(np.shape(subvolume)[0])-1,
-            step=1,
-            marks={i: f'{i}' for i in range(np.shape(subvolume)[0]) if i%5==0},
-            value=0,
-            id='x-slice-slider'
-            )
-    ]),
-    html.Div(children=[
-        html.H4('y-slice', style={'textAlign': 'center'}),
-        dcc.Graph(id='y-slice'),
-        dcc.Slider(
-            min=0,
-            max=(np.shape(subvolume)[1])-1,
-            step=1,
-            marks={i: f'{i}' for i in range(np.shape(subvolume)[1]) if i%5==0},
-            value=0,
-            id='y-slice-slider'
-            )
-    ]),
-    html.Div(children=[
-        html.H4('z-slice', style={'textAlign': 'center'}),
-        dcc.Graph(id='z-slice'),
-        dcc.Slider(
-            min=0,
-            max=(np.shape(subvolume)[2])-1,
-            step=1,
-            marks={i: f'{i}' for i in range(np.shape(subvolume)[2]) if i%5==0},
-            value=0,
-            id='z-slice-slider'
-            )
-    ]),
-    html.Div(
-        dcc.Graph(figure=vol_fig)
-    ),
+# TODO: Make these actually work
+controls = dbc.Card(
+    [
+        html.Div(
+            [
+                dbc.Label("X variable"),
+                dcc.Dropdown(
+                    id="color scheme",
+                    options=['rainbow', 'viridis'],
+                    value='rainbow',
+                ),
+            ]
+        ),
+        html.Div(
+            [
+                dbc.Label("max percentage"),
+                dbc.Input(id="max-percent", type="number", value=92),
+                
+            ]
+        ),
+        html.Div(
+            [
+                dbc.Label("min percentage"),
+                dbc.Input(id="min-percent", type="number", value=8),
+            ]
+        ),
+    ],
+    body=True,
+)
 
-#    html.Div(
-#        dbc.Row([
-#        dbc.Col(
-#            dash.html.Div(dash.dcc.Graph(figure=fig_heatmap0)), width={'size':3}
-#            ),
-#        dbc.Col(
-#            dash.html.Div(dash.dcc.Graph(figure=fig_heatmap1)), width={'size':3}
-#            ),
-#        dbc.Col(
-#            dash.html.Div(dash.dcc.Graph(figure=fig_heatmap1)), width={'size':3}
-#            )
-#        ])
-])
+
+app.layout = dbc.Container(
+    [
+        html.H2(f'{data_path}'),
+        html.Hr(),
+        dbc.Row(
+        [
+        dbc.Col(
+            
+                dbc.Row(
+                    [
+                    dcc.Graph(id='x-slice'),
+                    dcc.Slider(
+                        min=0,
+                        max=(np.shape(subvolume)[0])-1,
+                        step=1,
+                        marks={i: f'{i}' for i in range(np.shape(subvolume)[0]) if i%5==0},
+                        value=0,
+                        id='x-slice-slider'
+                        ),
+                     html.Hr(),
+                    dcc.Graph(id='y-slice'),
+                    dcc.Slider(
+                        min=0,
+                        max=(np.shape(subvolume)[1])-1,
+                        step=1,
+                        marks={i: f'{i}' for i in range(np.shape(subvolume)[1]) if i%5==0},
+                        value=0,
+                        id='y-slice-slider'
+                        ),
+                     html.Hr(),
+                    dcc.Graph(id='z-slice'),
+                    dcc.Slider(
+                        min=0,
+                        max=(np.shape(subvolume)[2])-1,
+                        step=1,
+                        marks={i: f'{i}' for i in range(np.shape(subvolume)[2]) if i%5==0},
+                        value=0,
+                        id='z-slice-slider'
+                        ),
+                     html.Hr(),
+                    ],
+                    align="center",
+                ),
+                md=6
+        ),
+        dbc.Col(
+            [
+                controls, 
+                html.Br(),
+                html.Br(),
+                dcc.Graph(figure=vol_fig),
+            ],
+            md=6),
+        ]
+        ),
+    ]
+)
+
+
+
 
 ##################################################
 ####### Callback functions for 2D slices #########
@@ -100,7 +132,8 @@ app.layout = html.Div(children=[
 def update_x_slice(input_value):
     return px.imshow(subvolume[input_value,:,:], 
                     zmin=np.percentile(subvolume, 8), 
-                    zmax=np.percentile(subvolume, 92))
+                    zmax=np.percentile(subvolume, 92),
+                    color_continuous_scale='viridis')
 
 
 @app.callback(
